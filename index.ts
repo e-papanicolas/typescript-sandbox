3bc0bf7931ec3225e53b434e9ac151030db09d1f
Eleni Papanicolas <76261375+e-papanicolas@users.noreply.github.com>
2022-03-25 20:11:55 -0400

    working with tuples and arrays

--- index.ts
+++ index.ts
@@ -1,3 +1,36 @@
 function myFirstTypescriptFunc(name: string): void {
   console.log(`Hi, ${name}`);
 }
+
+// tuples of arrays
+let monster: [
+  string[],
+  string[],
+  string[][],
+  string[][],
+  string[][][][],
+  string[][][][],
+  number[][][][][][],
+  number[],
+  string[],
+  boolean[],
+  number[],
+  string[],
+  boolean[],
+  number[][]
+] = [
+  [],
+  [],
+  [[]],
+  [[]],
+  [[[["X"]]]],
+  [[[["X"]]]],
+  [[[[[[1111111]]]]]],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [],
+  [[3]],
+];
